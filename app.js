bbeaa0aa20ea0f6e5d7abdd0afa55542149a2393
Mihir Aggarwal <mihir.aggarwal1000@gmail.com>
2021-04-19 20:44:20 +0530

    firebase

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const expressHandlebars = require('express-handlebars');
 const path = require('path');
+const firebase = require('firebase/app');
+require('firebase/auth');
+require('firebase/firestore');
+
+const PORT = process.env.PORT || 3000;
 
 const app = express();
+require('dotenv').config()
+
+const creds = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID
+}
+
+firebase.initializeApp(creds);
+
+let db = firebase.firestore();
 
 app.set('views', path.join(__dirname, '/views/'));
 
@@ -16,11 +36,20 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/static'));
 
 app.get('/play', (req, res) => {
-    res.render('play');
+    db.collection('questions').doc('q1').get()
+    .then((doc) => {
+        if (doc.exists) {
+            console.log(doc.data())
+            res.render('play', {data: {data: doc.data()}});
+        }
+        else { console.log('no data') }
+    }).catch((err) => {
+        console.error(err);
+    })
 });
 
 app.post('/play', (req, res) => {
     res.send('Posted')
 })
 
-app.listen(3000, console.log('Started'));
+app.listen(PORT, console.log('Started'));
